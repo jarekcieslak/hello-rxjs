e6ea5434127ddfff42254ec2196b2c2d747d0769
Lukas Ruebbelke <simpul@gmail.com>
2016-09-24 08:12:10 -0700

    basic observable sequence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.css'],
   template: `
-  <h1>
-    {{title}}
-  </h1>
+  <button #btn md-raised-button color="accent">Click me!</button>
+  <div class="container">
+    <h1>{{message}}</h1>
+  </div>
   `
 })
-export class AppComponent {
-  title = 'Hello RxJS!';
+export class AppComponent implements OnInit {
+  @ViewChild('btn') btn;
+  message: string;
+
+  ngOnInit() {
+    Observable.fromEvent(this.getNativeElement(this.btn), 'click')
+      .filter(event => event.shiftKey)
+      .map(event => 'Beast Mode Activated!!!')
+      .subscribe(message => this.message = message);
+  }
+
+  getNativeElement(element) {
+    return element._elementRef.nativeElement;
+  }
 }
