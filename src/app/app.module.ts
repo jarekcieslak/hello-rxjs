c6735ac98cebbbbe703b8a58bb3dc13801e19ee4
Lukas Ruebbelke <simpul@gmail.com>
2016-09-24 07:42:14 -0700

    initial checkin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MdButtonModule } from '@angular2-material/button';
+
 import { AppComponent } from './app.component';
+import { CircleComponent, LineComponent } from './shared';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CircleComponent,
+    LineComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
