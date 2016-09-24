a55f78b83f2a1568e2af59e755a4f21b6d57be0a
Lukas Ruebbelke <simpul@gmail.com>
2016-09-24 08:22:40 -0700

    maintaining state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,33 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/startWith';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.css'],
   template: `
-  <button #btn md-raised-button color="accent">Click me!</button>
+  <button #right md-raised-button color="accent">Move Right</button>
   <div class="container">
-    <h1>{{message}}</h1>
+    <div #ball class="ball"
+      [style.left]="position.x + 'px'"
+      [style.top]="position.y + 'px'">
+    </div>
   </div>
   `
 })
 export class AppComponent implements OnInit {
-  @ViewChild('btn') btn;
-  message: string;
+  @ViewChild('right') right;
+  position: any;
 
   ngOnInit() {
-    Observable.fromEvent(this.getNativeElement(this.btn), 'click')
-      .filter(event => event.shiftKey)
-      .map(event => 'Beast Mode Activated!!!')
-      .subscribe(message => this.message = message);
+    Observable.fromEvent(this.getNativeElement(this.right), 'click')
+      .map(event => 10)
+      .startWith({x: 400, y: 400})
+      .scan((acc, curr) => Object.assign({}, acc, {x: acc.x + curr}))
+      .subscribe(position => this.position = position);
   }
 
   getNativeElement(element) {
