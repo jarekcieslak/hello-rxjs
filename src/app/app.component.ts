871c9e0c689faf358730be61a2466ba9b47c951d
Lukas Ruebbelke <simpul@gmail.com>
2016-09-25 23:58:43 +0100

    realtime streams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,39 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { AngularFire } from 'angularfire2';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/observable/merge';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/pairwise';
 import 'rxjs/add/operator/scan';
 import 'rxjs/add/operator/startWith';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/takeUntil';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.css'],
   template: `
-  <app-line
-    *ngFor="let line of lines" [line]="line">
-  </app-line>
+  <button #btn md-raised-button color="accent">Click me!</button>  
+  <div>
+    <h2>Beast Mode Activated</h2>
+    <strong>{{count}} times!</strong>
+  </div>
   `
 })
 export class AppComponent implements OnInit {
-  lines: any[] = [];
-  ngOnInit() {
-    const emptyLine: any = { x1: 0, y1: 0, x2: 0, y2: 0 };
+  @ViewChild('btn') btn;
 
-    // Observable.fromEvent(document, 'mousemove')
-    Observable.fromEvent(document, 'click')
-      .map((event: any) => {
-        const offset = $(event.target).offset();
-        return {
-          x: event.clientX - offset.left,
-          y: event.pageY - offset.top
-        };
-      })
-      .pairwise(2)
-      .map(positions => {
-        const p1 = positions[0];
-        const p2 = positions[1];
-        return { x1: p1.x, y1: p1.y, x2: p2.x, y2: p2.y };
-      })
-      .startWith(emptyLine)
-      .subscribe(line => this.lines = [...this.lines, line]);
+  constructor(private af: AngularFire) {}
+
+  ngOnInit() {
+    const remote$ = this.af.database.object('clicker/');
+
+    Observable.fromEvent(this.getNativeElement(this.btn), 'click')
+      .startWith({ticker: 0})
+      .scan((acc, curr) => { return { ticker: acc.ticker + 1 }; })
+      .subscribe(event => remote$.update(event));
+
+    remote$
+      .subscribe(result => this.count = result.ticker);
+  }
+
+  getNativeElement(element) {
+    return element._elementRef.nativeElement;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import * as firebase from 'firebase';
+import { AngularFireModule } from 'angularfire2';
+
 import { MdButtonModule } from '@angular2-material/button';
 
 import { AppComponent } from './app.component';
 import { CircleComponent, LineComponent } from './shared';
 
+export const firebaseConfig = {
+  apiKey: 'PUT_API_KEY_HERE',
+  authDomain: 'PUT_DOMAIN_HERE',
+  databaseURL: 'PUT_URL_HERE',
+  storageBucket: ''
+};
 
 @NgModule({
   declarations: [
@@ -17,6 +26,7 @@ import { CircleComponent, LineComponent } from './shared';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
     HttpModule,
     MdButtonModule
