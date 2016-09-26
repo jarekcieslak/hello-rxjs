2c5aec99e71eb6fb64c5ec46778717ca597f69b9
Lukas Ruebbelke <simpul@gmail.com>
2016-09-26 01:11:02 +0100

    chore: clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/observable/merge';
