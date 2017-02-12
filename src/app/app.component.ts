7583b5a0276d11c85cbdfd8f0914e20ae188b118
Jaroslaw Cieslak <jarekcieslak@gmail.com>
2017-02-12 23:06:07 +0100

    Basic observable stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Observable} from 'rxjs/Observable'
+import 'rxjs/add/observable/fromEvent'
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['./app.component.css'],
-  template: `
+    selector: 'app-root',
+    styleUrls: ['./app.component.css'],
+    template: `
   <button #btn md-raised-button color="accent">Click me!</button>
   <div class="container">
     <h1>{{message}}</h1>
@@ -11,13 +13,17 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   `
 })
 export class AppComponent implements OnInit {
-  @ViewChild('btn') btn;
-  message: string;
+    @ViewChild('btn') btn;
+    message: string;
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        Observable.fromEvent(this.getNativeElement(this.btn), 'click').subscribe(event => {
+            this.btn.clientX = 0;
+            console.log(event)
+        });
+    }
 
-  getNativeElement(element) {
-    return element._elementRef.nativeElement;
-  }
+    getNativeElement(element) {
+        return element._elementRef.nativeElement;
+    }
 }
